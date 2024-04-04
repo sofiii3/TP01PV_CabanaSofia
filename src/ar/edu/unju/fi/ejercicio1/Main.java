feaa81093b8817a1606a49f644adfacb0f965615
caban <caban@sofia3>
2024-04-04 15:17:57 -0300

    resuelo1_cabanasofia

---
+++ src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package ar.edu.unju.fi.ejercicio1;
+
+/**
+ * 
+ */
+public class Main {
+	
+	public static void main(String[] args) {
+		/*
+		int numero01 = 5;
+		int numero02 = 6;
+		double promedio = (double)(numero01 + numero02)/2;
+		
+		System.out.println(promedio);
+		*/
+		float primerNumero = 8;
+		float segundoNumero = 5;
+		float tercerNumero = 1;
+		float cuartoNumero = 20;
+		float quintoNumero = 9;
+		float promedio = (primerNumero + segundoNumero + tercerNumero + cuartoNumero + quintoNumero)/ 5;
+		System.out.println(primerNumero+", "+segundoNumero);
+		System.out.println("El promedio es: "+ promedio );
+	}
+
+}
